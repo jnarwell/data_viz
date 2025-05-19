77857bdd47886e21d507b49a02ffc794c5c968d7
James Marwell <jcmarwell@gmail.com>
2025-05-18 22:16:16 -0700

    hheee

--- docs/script.js
+++ docs/script.js
@@ -1,13 +1,11 @@
-(async () => {
-  const url = 'https://docs.google.com/spreadsheets/d/SHEET_ID/gviz/tq?tqx=out:json';
-  async function fetchData() {
-    const res = await fetch(url);
-    const text = await res.text();
-    const json = JSON.parse(text.substring(47, text.length - 2));
-    const cols = json.table.cols.map(c => c.label);
-    const rows = json.table.rows.map(r => r.c.map(c => c ? c.v : null));
-    const data = rows.map(r => Object.fromEntries(r.map((v, i) => [cols[i], v])));
-    console.log(data);
-  }
-  await fetchData();
-})();
+async function main() {
+  let pyodide = await loadPyodide();
+  await pyodide.loadPackage("micropip");
+  await pyodide.runPythonAsync(`
+import micropip
+await micropip.install("matplotlib")
+import matplotlib.pyplot as plt
+# now safe to use matplotlib in your embedded Python
+`);
+}
+main();
